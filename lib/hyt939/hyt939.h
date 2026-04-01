b5736fa091e165dba0392b73f1df15b6f3cd03cf
Shannon Reyon <shannonjayakody52@gmail.com>
2026-04-02 01:27:45 +0200

    test with rtos adapted new hyt939 drivers

--- lib/hyt939/hyt939.h
+++ lib/hyt939/hyt939.h
@@ -8,23 +8,7 @@ class HYT939
 {
 public:
     HYT939();
-    /*
 
-    // default HYT address is 0x28
-    bool begin(TwoWire &wire = Wire, uint8_t address = 0x28);
-
-    // Read full measurement (status + humidity + temperature)
-    // status: 0=normal, 1=stale/old data, 2=command mode, 3=diagnostic
-    bool read(uint8_t &status, float &rh_percent, float &temp_c);
-
-    // Low-level read (raw 4 bytes)
-    bool readRaw(uint8_t data[4]);
-
-    // Convenience functions
-    float readHumidityPercent(uint8_t *outStatus = nullptr);
-    float readTemperatureC(uint8_t *outStatus = nullptr);
-
-    */
     bool begin(TwoWire &wire, uint8_t address = 0x28);
     bool trigger();
     bool readResult(uint8_t &status, float &rh_percent, float &temp_c);
